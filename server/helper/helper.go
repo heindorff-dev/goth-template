7df88c0886d9c8cb2a57e65445831249a646928c
heindorff-dev <andreas.heindorff@proton.me>
2025-03-01 23:08:46 +0100

    cleanup

--- server/helper/helper.go
+++ server/helper/helper.go
@@ -1,20 +1,10 @@
 package helper
 
 import (
-	"errors"
 	"os"
 
 	"github.com/joho/godotenv"
 )
-
-func GetEnv(key string) (string, error) {
-	envValue := os.Getenv(key)
-	if envValue == "" {
-		errMessage := "The environment variable '" + key + "' doesn't exist or is not set."
-		return "", errors.New(errMessage)
-	}
-	return envValue, nil
-}
 
 func MustGetEnv(key string) string {
 	err := godotenv.Load()
